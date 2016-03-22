d2a2409b50e075d9b261f2cc84fb599a3b2d36e3
LandRover <github.com@landroverz.com>
2016-03-22 06:18:59 +0000

    text

--- test/utils/encoder.js
+++ test/utils/encoder.js
@@ -12,6 +12,30 @@ chai.should();
 chai.use(sinonChai);
 
 
+const TEXT = {
+    BOOL_TRUE: true,
+    BOOL_FALSE: false,
+    
+    STRING1: '8.000.0',
+    STRING2: '08f60ae5cf4500ccc86a0601c34855ce',
+    
+    BYTE0: 0,
+    BYTE1: 1,
+    BYTE5: 5,
+    BYTE9: 9
+    
+    
+    INT1: 12345678,
+    INT2: 1234,
+    INT3: 9999,
+    
+    LONG1: 12345678012345678,
+    LONG2: 11111111111111111,
+    LONG3: 1111,
+    LONG4: 9999
+};
+
+
 describe('Encoder testing types', () => {
     beforeEach(() => {
     });
