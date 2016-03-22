8303a3baf840f0d744ec7c6815a2fcb6ec78be2f
LandRover <github.com@landroverz.com>
2016-03-22 17:01:14 +0000

    comments

--- src/utils/encoder.js
+++ src/utils/encoder.js
@@ -6,16 +6,25 @@ class Encoder {
     constructor() {}
 
 
+    /**
+     *
+     **/
     int(number) {
         return this._DWORD(number);
     }
 
 
+    /**
+     *
+     **/
     long(number) {
         return this._QWORD(number);
     }
 
 
+    /**
+     *
+     **/
     string(str) {
         let payloadLength = Buffer.byteLength(str),
             buffer = this._buffer(payloadLength, DATA_TYPES.INT32_LEN + payloadLength, DATA_TYPES.INT32_LEN);
@@ -26,6 +35,9 @@ class Encoder {
     }
 
 
+    /**
+     *
+     **/
     boolean(flag) {
         let state = 0;
 
@@ -37,21 +49,33 @@ class Encoder {
     }
 
 
+    /**
+     *
+     **/
     byte(digit) {
         return this._buffer(digit, DATA_TYPES.BYTE_LEN);
     }
 
 
+    /**
+     *
+     **/
     _DWORD(number) {
         return this._buffer(number, DATA_TYPES.INT32_LEN);
     }
 
 
+    /**
+     *
+     **/
     _QWORD(number) {
         return this._buffer(number, DATA_TYPES.INT64_LEN);
     }
 
 
+    /**
+     *
+     **/
     _buffer(number, len, position) {
         position = position || len; // default position is at the end of the buffer, unless is set for something else manually.
 
